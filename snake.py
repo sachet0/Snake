ccba043b041f5c9295141b48c7db2a2acabc19cf
Sachet <sachet0@users.noreply.github.com>
2021-01-08 19:32:09 -0500

    files to push

--- snake.py
+++ snake.py
@@ -12,50 +12,32 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 
 screen = pygame.display.set_mode(res)
-# pygame.display.set_caption("Snake")
-# pygame.display.set_icon(pygame.image.load("tux.png"))
-# fruit = pygame.draw.rect(screen, red, (100,100,15,15), width=0)
+pygame.display.set_caption("Snake")
+# fruit = pygame.draw.rect(screen, white, (100,100,15,15), width=0)
 # snake = pygame.draw.rect(screen, white, (250,250,20,20), width=0)
-
-# PyInstaller required code block
-def resource_path(relative_path):
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
-
 
 posX = 240
 posY = 240
 
-snakeSurf = pygame.Surface((15, 15))
+snakeSurf = pygame.Surface((20, 20))
 snakeSurf.fill(white)
 snakeRect = snakeSurf.get_rect()
-snakeRect.move_ip(posX, posY)
-screen.blit(snakeSurf, snakeRect)
-
-
-def move(keyPress):
-    if keyPress == K_UP and snakeRect[1] > 0:
-        snakeRect.move_ip(0, -15)
-    if keyPress == K_DOWN and snakeRect[1] < 480:
-        snakeRect.move_ip(0, 15)
-    if keyPress == K_LEFT and snakeRect[0] > 0:
-        snakeRect.move_ip(-15, 0)
-    if keyPress == K_RIGHT and snakeRect[0] < 480:
-        snakeRect.move_ip(15, 0)
-    draw()
-
-
-def draw():
-    screen.blit(snakeSurf, snakeRect)
-    print("draw called")
-
+snakeRect.x = posX
+snakeRect.y = posY
 
 keypress = None
 oldkey = keypress
+
+def move(keyPress):
+    if keyPress == K_UP and snakeRect[1] > 0:
+        snakeRect.move_ip(0, -20)
+    if keyPress == K_DOWN and snakeRect[1] < 480:
+        snakeRect.move_ip(0, 20)
+    if keyPress == K_LEFT and snakeRect[0] > 0:
+        snakeRect.move_ip(-20, 0)
+    if keyPress == K_RIGHT and snakeRect[0] < 480:
+        snakeRect.move_ip(20, 0)
+    screen.blit(snakeSurf, snakeRect)
 
 while True:
     for event in pygame.event.get():
@@ -64,6 +46,7 @@ while True:
             sys.exit()
 
         if event.type == KEYDOWN:
+            print(keypress)
             oldkey = keypress
             if event.key == K_UP and oldkey != K_DOWN:
                 keypress = event.key
@@ -77,4 +60,13 @@ while True:
     screen.fill(black)
     move(keypress)
     pygame.display.update()
-    FPS.tick(5)
+    FPS.tick(8)
+
+# PyInstaller required code block
+def resource_path(relative_path):
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
